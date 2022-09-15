90d4e304d7b8ad26629a97fe9b45830b14a0496a
Nik <nczyabo@gmail.com>
2022-09-16 01:21:01 +0800

    responsive fix

--- resources/views/layouts/menufooter.blade.php
+++ resources/views/layouts/menufooter.blade.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> 
     <!-- Jquery and tailwind--> 
-    @vite('./resources/css/app.css')
+    @vite('/resources/css/app.css')
     <style>
         html{
             font-family: 'Manrope', sans-serif;  
